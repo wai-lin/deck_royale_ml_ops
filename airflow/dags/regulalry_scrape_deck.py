83c269226fbcfdc634e957d11663f774cef2ce4b
wai-lin <dev.wailin@gmail.com>
2025-06-26 23:48:56 +0700

    airflow initialized

---
+++ airflow/dags/regulalry_scrape_deck.py
@@ -0,0 +1,21 @@
+import requests
+import datetime
+from airflow import DAG
+from airflow.decorators import task
+
+# @dag(
+#     schedule="0 9 * * *",  # Daily at 9 AM
+#     start_date=datetime(2025, 1, 1, tz="UTC"),
+#     catchup=False,
+#     tags=["papers", "research", "mlops", "daily"],
+# )
+with DAG(
+        schedule="0 9 * * *",  # Daily at 9 AM
+        start_date=datetime(2025, 1, 1, tz="UTC")
+        dag_id="regularly_scrape_deck") as dag:
+    @task()
+    def scrape_deck():
+        requests.get("http://localhost:8000/decks")
+
+    scrape_deck()
+
