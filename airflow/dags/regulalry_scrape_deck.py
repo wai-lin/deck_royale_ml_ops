a4efa149029c3336307739cbf348e7527fa8c85a
wai-lin <dev.wailin@gmail.com>
2025-06-27 02:00:56 +0700

    remove dag comments

--- airflow/dags/regulalry_scrape_deck.py
+++ airflow/dags/regulalry_scrape_deck.py
@@ -3,16 +3,12 @@ import datetime
 from airflow import DAG
 from airflow.decorators import task
 
-# @dag(
-#     schedule="0 9 * * *",  # Daily at 9 AM
-#     start_date=datetime(2025, 1, 1, tz="UTC"),
-#     catchup=False,
-#     tags=["papers", "research", "mlops", "daily"],
-# )
+
 with DAG(
         schedule="0 9 * * *",  # Daily at 9 AM
-        start_date=datetime(2025, 1, 1, tz="UTC")
+        start_date=datetime(2025, 1, 1, tz="UTC"),
         dag_id="regularly_scrape_deck") as dag:
+
     @task()
     def scrape_deck():
         requests.get("http://localhost:8000/decks")
