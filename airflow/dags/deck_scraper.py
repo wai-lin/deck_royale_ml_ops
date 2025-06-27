21c4c198f8e2954cf5781d9fd5b94e1778edd933
wai-lin <dev.wailin@gmail.com>
2025-06-27 09:14:04 +0700

    scraper dag error fixed

--- airflow/dags/deck_scraper.py
+++ airflow/dags/deck_scraper.py
@@ -1,17 +1,17 @@
 import requests
-import datetime
+from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task
 
 
 with DAG(
         schedule="0 9 * * *",  # Daily at 9 AM
-        start_date=datetime(2025, 1, 1, tz="UTC"),
+        start_date=datetime(2025, 1, 1),
         dag_id="regularly_scrape_deck") as dag:
 
     @task()
     def scrape_deck():
-        requests.get("http://localhost:8000/decks")
+        requests.get("http://host.docker.internal:8000/decks")
 
     scrape_deck()
 
